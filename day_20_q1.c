4e9f27dbc0f0c0314086545af61ed6602788d964
suhanaa29 <aparnasingh29062006@gmail.com>
2025-09-09 11:29:24 +0530

    C program to compute product of odd digits

---
+++ day_20_q1.c
@@ -0,0 +1,44 @@
+// Q39: Write a program to find the product of odd digits of a number.
+
+/*
+Sample Test Cases:
+Input 1:
+12345
+Output 1:
+15 (1*3*5)
+
+Input 2:
+2468
+Output 2:
+1 (no odd digits, assume 1)
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    int num, digit;
+    int product = 1;
+    int hasOdd = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    while (num > 0) {
+        digit = num % 10;
+        if (digit % 2 != 0) {
+            product *= digit;
+            hasOdd = 1;
+        }
+        num /= 10;
+    }
+
+    if (!hasOdd) {
+        product = 1; // No odd digits found
+    }
+
+    printf("Product of odd digits: %d\n", product);
+    return 0;
+}
+
+
