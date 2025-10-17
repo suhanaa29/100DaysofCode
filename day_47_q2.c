7024b8255497d68f3a63902d97d5c7a9b84ae024
suhanaa29 <aparnasingh29062006@gmail.com>
2025-10-17 18:04:36 +0530

    update progras

--- day_47_q2.c
+++ day_47_q2.c
@@ -10,39 +10,53 @@ programming
 */
 
 
+
 #include <stdio.h>
-#include <conio.h>
 #include <string.h>
+#include <ctype.h>
 
-int main()
-{
-    char s[100],w[50],lw[50];
-    int i,p;
+int main() {
+    char str[100];
+    int i, len;
 
-    printf("Enter a sentence\n");
-    gets(s);
+    printf("Enter a sentence: ");
+    if (fgets(str, sizeof(str), stdin) == NULL) {
+        printf("Input error!\n");
+        return 1;
+    }
 
-    for(i=0; i<strlen(s); i++)
-    {
-        if(s[i]!=' ')
-        {
-            // store the characters in array w until we find a space
-            w[p]=s[i];
-            p++;
-        }
-        else
-        {
-            // terminate the word by a null character
-            w[p]='\0';
+    // Remove trailing newline if present
+    len = strlen(str);
+    if (len > 0 && str[len - 1] == '\n') {
+        str[len - 1] = '\0';
+        len--;
+    }
 
-            // check if word length is greater than longest word (lw)
-            if(strlen(w)>strlen(lw))
-            {
-                strcpy(lw,w);
+    int maxLength = 0, currentLength = 0;
+    char longestWord[100] = "";   // initialize to empty string
+    char currentWord[100];
+
+    for (i = 0; i <= len; i++) {
+        if (isalpha((unsigned char)str[i])) {
+            currentWord[currentLength++] = str[i];
+        } else {
+            if (currentLength > 0) {
+                currentWord[currentLength] = '\0';
+                if (currentLength > maxLength) {
+                    maxLength = currentLength;
+                    strcpy(longestWord, currentWord);
+                }
+                currentLength = 0;
             }
-            p=0;
         }
     }
-    printf("Longest word = %s",lw);
+
+    if (maxLength > 0) {
+        printf("Longest word: %s\n", longestWord);
+        printf("Length: %d\n", maxLength);
+    } else {
+        printf("No valid words found.\n");
+    }
+
     return 0;
 }
